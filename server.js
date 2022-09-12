356fe932ab7779c29d79b08f7417d9d9ee0f13b7
João Hudson <joao.oliveira@projecti.com.br>
2022-09-12 15:53:53 -0300

    corrigindo busca por ip

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/api/search', async (req, res) => {
 });
 
 app.get('/api/ip', async (req, res) => {
-    res.send(req.hostname);
+    res.send(request.socket.remoteAddress);
 });
 
 app.get('*', (req, res) => {
